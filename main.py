5e58f7292c28c3e002f54287ac81f60482388c76
Kirill Morozov <kmmorozov@gmail.com>
2022-11-03 19:43:37 +0300

    Второй коммит

---
+++ main.py
@@ -0,0 +1,21 @@
+def read_data_file(file_name):
+    file_lines = []
+    file = open(file_name, 'r')
+    for line in file:
+        file_lines.append(line)
+    return file_lines
+
+
+def split_line(line):
+    splited_line = line.split(';')
+    return splited_line
+
+
+if __name__ == '__main__':
+    stroki_iz_faila = read_data_file('data.csv')
+    for stroka in stroki_iz_faila:
+        razbitaya_stroka = split_line(stroka)
+        # print(razbitaya_stroka)
+        print(
+            f'Меня зовут {razbitaya_stroka[0]} {razbitaya_stroka[1]}, мой пол {razbitaya_stroka[2]} год моего рождения {razbitaya_stroka[3]},'
+            f' можете мне звонить по телефону {razbitaya_stroka[4]}')
---
+++ main2.py
@@ -0,0 +1,19 @@
+
+
+if __name__ == '__main__':
+    file_lines = []
+    file = open('data.csv', 'r')
+    for line in file:
+        file_lines.append(line)
+    stroki_iz_faila = file_lines
+    print(stroki_iz_faila)
+
+    file_lines = []
+    file = open('data2.csv', 'r')
+    for line in file:
+        file_lines.append(line)
+    stroki_iz_faila = file_lines
+
+
+
+
