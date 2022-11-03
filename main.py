1ede20c05219c5d41dc2adaab2ab7f4b046b3fd8
Kirill Morozov <kmmorozov@gmail.com>
2022-11-03 21:43:31 +0300

    добавил работу с db

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import pymysql
+
+
 def read_data_file(file_name):
     file_lines = []
     file = open(file_name, 'r')
@@ -11,11 +14,18 @@ def split_line(line):
     return splited_line
 
 
+def connect_to_db(host, port, db_name, user, password):
+    connection = pymysql.connect(host=host, user=user,
+                                 port=port, db=db_name, password=password)
+    cursor = connection.cursor()
+    return cursor,connection
+
+
 if __name__ == '__main__':
+    cursor, connection = connect_to_db('10.10.101.183',3306,'test','student','123456')
     stroki_iz_faila = read_data_file('data.csv')
     for stroka in stroki_iz_faila:
         razbitaya_stroka = split_line(stroka)
-        # print(razbitaya_stroka)
-        print(
-            f'Меня зовут {razbitaya_stroka[0]} {razbitaya_stroka[1]}, мой пол {razbitaya_stroka[2]} год моего рождения {razbitaya_stroka[3]},'
-            f' можете мне звонить по телефону {razbitaya_stroka[4]}')
+        cursor.execute("insert into sprav values('{}','{}','{}','{}','{}');".format(razbitaya_stroka[0],razbitaya_stroka[1],razbitaya_stroka[2],razbitaya_stroka[3],razbitaya_stroka[4].strip()))
+
+    connection.commit()
